69ff7578bc54bd623e678e982295febc29ccc46d
Alessandro <sandroledesma@gmail.com>
2024-04-30 10:28:01 -0400

    add new files

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+/*
+Your job is to write the following function:
+titleCased(): returns an array with title case tutorial names. 
+Note that this function takes no arguments and should use the global tutorials variable as data.
+
+NOTE: This lab is challenging! You will need to iterate through the tutorials array, modifying the name of each tutorial. 
+To do this, you will also need to access and modify each individual word.
+
+Some questions to consider:
+> How can we "iterate" through individual words in a string?
+> Can we execute an iteration inside an iteration? How?
+> How can we capitalize just the first letter in a word?
+
+*/
+
 const tutorials = [
   'what does the this keyword mean?',
   'What is the Constructor OO pattern?',
@@ -11,6 +26,27 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
-}
+function titleCased(tutorials) {
+  return tutorials.map(tutorial => {
+    return tutorial.split(" ").map(word => {
+      const capitalizedFirstLetter = word.charAt(0).toUpperCase();
+      const restOfWord = word.slice(1);
+      return capitalizedFirstLetter + restOfWord;
+    }).join(" ");
+  })
+} 
+
+titleCased(tutorials);
+console.log(titleCased(tutorials));
+
+/*
+function displayTutorials() { //this function is needed in order to create a list of tutorials on the page
+  const titleCasedTutorials = titleCased(tutorials);
+  const tutorialsListDiv = document.getElementById("tutorialList");
+
+  titleCasedTutorials.forEach(title => {
+    const paragraph = document.createElement("body");
+    paragraph.textContent = title;
+    tutorialsListDiv.appendChild(paragraph);
+  });
+} */
--- test/indexTest.js
+++ test/indexTest.js
@@ -3,7 +3,7 @@ require ( './helpers.js' );
 describe('index.js', function () {
   describe('titleCased()', function () {
     it('returns an array with title case tutorial names', function () {
-      expect(titleCased()).to.have.all.members(
+      expect(titleCased(tutorials)).to.have.all.members(
         [
           "What Does The This Keyword Mean?",
           "What Is The Constructor OO Pattern?",
